033e55d69fce25192776a531aea5472415b643c9
daniel <danielpalma232@gmail.com>
2022-01-12 17:34:18 -0300

    Tentativa de arrumar h3 para mobile com JS

--- js/index.js
+++ js/index.js
@@ -7,3 +7,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
   });
 });
+
+let textH3 = document.getElementById('text_h3')
+console.log(document.body.clientWidth)
+
+if(document.body.clientWidth >= 414) {
+  textH3.innerHTML = '<a class="link_instagram" href="https://www.instagram.com/daniel.dpalma/" target="_blank" >Daniel Dario Palma</a> | 22 anos | Campinas, SP'
+} else {
+  textH3.innerHTML = '<a class="link_instagram" href="https://www.instagram.com/daniel.dpalma/" target="_blank" >Daniel Dario Palma</a> | 22 anos Campinas, SP'
+}
+
+
