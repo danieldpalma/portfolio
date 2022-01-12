705a64d196bb6322038428dd9c003a1a4dbce63b
daniel <danielpalma232@gmail.com>
2022-01-12 17:47:49 -0300

    Alterado linha do h3 para melhor posição

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// const instagram = '<a class="link_instagram" href="https://www.instagram.com/daniel.dpalma/" target="_blank" >Daniel Dario Palma</a>'
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -8,13 +9,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-let textH3 = document.getElementById('text_h3')
-console.log(document.body.clientWidth)
+// let textH3 = document.getElementById('text_h3')
+// console.log(document.body.clientWidth)
 
-if(document.body.clientWidth >= 414) {
-  textH3.innerHTML = '<a class="link_instagram" href="https://www.instagram.com/daniel.dpalma/" target="_blank" >Daniel Dario Palma</a> | 22 anos | Campinas, SP'
-} else {
-  textH3.innerHTML = '<a class="link_instagram" href="https://www.instagram.com/daniel.dpalma/" target="_blank" >Daniel Dario Palma</a> | 22 anos Campinas, SP'
-}
+// if(document.body.clientWidth > 390) {
+//   textH3.innerHTML =  `${instagram} | 22 anos<br>Campinas, SP`
+// } else {
+//   textH3.innerHTML =  `${instagram} | 22 anos<br>Campinas, SP`
+// }
 
 
